1b64bafb4900860c1069f07a6895745e4fed470b
Axio Dev <axelvazmar0@gmail.com>
2026-07-16 12:42:55 -0600

    feat(users): añade un endpoint de logout para terminar la sesión

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterAPIView, CheckUserStatusAPIView
+from .views import LoginAPIView, RegisterAPIView, CheckUserStatusAPIView, LogoutAPIView
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("register/", RegisterAPIView.as_view(), name="register"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("check-status/", CheckUserStatusAPIView.as_view(), name="user-status"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -51,6 +51,16 @@ class LoginAPIView(APIView):
                 **tokens,
             },
             status=status.HTTP_200_OK,
+        )
+
+
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response(
+            {"message": "user logout successfully"}, status=status.HTTP_200_OK
         )
 
 
